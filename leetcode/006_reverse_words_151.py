53a4d4543c2f86f6180461304d3effcbbd48f3a5
Sumeet Shukla <sumeet.k.shukla@gmail.com>
2024-01-04 12:47:42 +0530

    feat: leetcode 151

---
+++ leetcode/006_reverse_words_151.py
@@ -0,0 +1,22 @@
+# 151. Reverse Words in a String
+
+# Given an input string s, reverse the order of the words.
+# A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+# Return a string of the words in reverse order concatenated by a single space.
+# Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+def reverseWordsOne(s):
+    lst_s = s.split()
+    lst_s.reverse()
+    return ' '.join(lst_s)
+
+
+print(reverseWordsOne('the sky is blue'))
+
+
+# Approach2
+def reverseWordsTwo(s):
+    return " ".join(s.split()[::-1])
+
+
+print(reverseWordsTwo('a good   example'))
