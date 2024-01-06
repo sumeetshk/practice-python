676c53dce3611292710cb75370a8db72e0d5289b
Sumeet Shukla <sumeet.k.shukla@gmail.com>
2024-01-06 17:58:30 +0530

    feat: leetcode 334

---
+++ leetcode/008_increasing_triplet_subsequence_334.py
@@ -0,0 +1,21 @@
+# 334. Increasing Triplet Subsequence
+
+# Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k].
+# If no such indices exists, return false.
+
+import itertools
+
+
+def increasingTriplet(nums):
+    first = second = float('inf')
+    for n in nums:
+        if n <= first:
+            first = n
+        elif n <= second:
+            second = n
+        else:
+            return True
+    return False
+
+
+print(increasingTriplet([1, 2, 3, 4, 5,7,4,8]))
